3693489811a362d10db10180590d4fd18c70c642
PARI AGARWAL1500 <86663513+PariAGARWAL148@users.noreply.github.com>
2022-04-24 15:45:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,131 @@
-menu_list_array = ["Veg Margherita Pizza",
-                        "Chicken Tandoori Pizza",
-                        "Veg Supreme Pizza",
-                        "Paneer Tikka Pizza",
-                        "Deluxe Veggie Pizza",
-                        "Veg Extravaganza Pizza"];
-    
-    function getmenu(){
-        var htmldata="";
-        menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
-        }
-        document.getElementById("display_menu").innerHTML = htmldata;
-        //give the appropriate id name as display_menu 
-    }
 
-    function add_item(){
-        var htmldata;
-		var imgtags='<img id="im1" src="pizzaImg.png">'
-        var item=document.getElementById("add_item").value;
-        menu_list_array.sort();
-        // use the sort function as - menu_list_array.sort();
-        htmldata="";
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
-        }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;
-		
-    }
+// Create canvas variable
+var canvas = new fabric. canvas('myCanvas');
 
-function add_top(){
-	var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-	add_item();
+//Set initial positions for ball and hole images.
+ball_y=0
+ball_x=0
+hole_y=400
+hole_x=800
+
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	// write code to Upload golf image on the canvas
+	function load_img(){
+fabric.image.fromURL("golf-h1.png",function(img){
+	hole_obj=img;
+	hole_obj.scaleToWidth(50);
+	hole_obj.scaleToHeight(50);
+	hole_obj.set({
+		top:hole_y,
+		left:hole_x
+	});
+	canvas.add(hole_obj);
+});
+new_image();	
 }
+	new_image();
+}
+
+function new_image()
+{
+	// write code to Upload ball image on canvas
+	function new_image(){
+		fabric.image.fromURL("ball.png",function(img){
+			ball_obj=img;
+			ball_obj.scaletowidth(50);
+			ball_obj.scaleToheight(50);
+			ball_obj.set({
+			top:ball_y,
+			left:ball_x	
+			});
+			canvas.add(ball_obj);
+		});
+	}
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
+	if((ball_x==hole_x)&&(ball_y==hole_y)){
+	canvas.remove(ball_obj);
+	}
+	document.getElementById("hd3").innerHTML="you have hit the goal!!!";
+	document.getElementById("myCanvas").style.borderColor="red;"
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		// Write a code to move ball upward.
+
+		
+	}
+
+	function down()
+	{
+		 // Write a code to move ball downward.
+		 function down()
+		 {
+		if(ball_y <=450)
+		{
+			ball_y=ball_y+block_image_height;
+			console.log("block image height="+block_image_height);
+			console.log("when down arrow key is pressed, x="+ ball_x +",y= "+ball_y);
+			canvas.remove(ball_obj);
+			new_image();
+		}	 
+		 }
+		 
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+		}
+	}
+	
+}
+
