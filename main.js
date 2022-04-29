18ea0f3982788c5aea9eb75a84a898b6cd96ae8e
PARI AGARWAL1500 <86663513+PariAGARWAL148@users.noreply.github.com>
2022-04-29 21:18:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,47 @@
+//Create a reference for canvas 
+canvas=document.getElementById('myCanvas');
 
-// Create canvas variable
-var canvas = new fabric. canvas('myCanvas');
+//Give specific height and width to the car image
+greencar_width=75;
+greencar_height=100;
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
 
-//Set initial positions for ball and hole images.
-ball_y=0
-ball_x=0
-hole_y=400
-hole_x=800
+//Set initial position for a car image.
+greencar_x=5;
+greencar_y=225;
 
 
-block_image_width = 5;
-block_image_height = 5;
+	//upload car, and background images on the canvas.
+	function add() {
+		backgroundimgTag=new Image()
+		background_imgTag.onload=uploadBackground;
+		background_imgTag.src=background_image;
 
-function load_img(){
-	// write code to Upload golf image on the canvas
-	function load_img(){
-fabric.image.fromURL("golf-h1.png",function(img){
-	hole_obj=img;
-	hole_obj.scaleToWidth(50);
-	hole_obj.scaleToHeight(50);
-	hole_obj.set({
-		top:hole_y,
-		left:hole_x
-	});
-	canvas.add(hole_obj);
-});
-new_image();	
-}
-	new_image();
+		greencar_imgTag=new Image();
+		greencar_imgTag.onload=uploadgreencar;
+		greencar_imgTag.src=greencar_image;
+
+	
 }
 
-function new_image()
-{
-	// write code to Upload ball image on canvas
-	function new_image(){
-		fabric.image.fromURL("ball.png",function(img){
-			ball_obj=img;
-			ball_obj.scaletowidth(50);
-			ball_obj.scaleToheight(50);
-			ball_obj.set({
-			top:ball_y,
-			left:ball_x	
-			});
-			canvas.add(ball_obj);
-		});
+function uploadBackground() {
+	//Define function ‘uploadBackground’
+	function uploadBackground(){
+		AudioContext.drawImage(background_imgTag,0,0,canvas.width,canavs.height);
 	}
+	function uploadgreencar(){
+		AudioContext.drawImage(greencar_imgTag,greencar_x,greencar_y,greencar_width,greencar_height);
+	}
+
 }
+
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+
+	
+}
+
 
 window.addEventListener("keydown", my_keydown);
 
@@ -53,79 +49,58 @@ function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	/* Check the coordinates of the ball and hole images to finish the game. 
-	And id coordinates matches them remove ball image, 
-	display "GAME OVER!!!" 
-	and make canvas border 'red'. */
-	if((ball_x==hole_x)&&(ball_y==hole_y)){
-	canvas.remove(ball_obj);
-	}
-	document.getElementById("hd3").innerHTML="you have hit the goal!!!";
-	document.getElementById("myCanvas").style.borderColor="red;"
-	
-	else{
 		if(keyPressed == '38')
 		{
 			up();
 			console.log("up");
 		}
+	
 		if(keyPressed == '40')
 		{
 			down();
 			console.log("down");
 		}
+		
 		if(keyPressed == '37')
 		{
 			left();
 			console.log("left");
 		}
+	
 		if(keyPressed == '39')
 		{
 			right();
 			console.log("right");
 		}
-	}
-	
-	function up()
-	{
-		// Write a code to move ball upward.
-
 		
-	}
-
-	function down()
-	{
-		 // Write a code to move ball downward.
-		 function down()
-		 {
-		if(ball_y <=450)
-		{
-			ball_y=ball_y+block_image_height;
-			console.log("block image height="+block_image_height);
-			console.log("when down arrow key is pressed, x="+ ball_x +",y= "+ball_y);
-			canvas.remove(ball_obj);
-			new_image();
-		}	 
-		 }
-		 
-	}
-
-	function left()
-	{
-		if(ball_x >5)
-		{
-			// Write a code to move ball left side.
-
-		}
-	}
-
-	function right()
-	{
-		if(ball_x <=1050)
-		{
-			// Write a code to move ball right side.
-		}
-	}
-	
+		
 }
 
+function up()
+{
+	//Define function to move the car upward
+	function up(){
+		if(greencar_y>=0) {
+			greencar_y=greencar_y-10;
+			console.log("when up arrow is pressed, x="+greencar_x"| y="+greencar_y);
+			uploadBackground();
+			uploadBackground();
+		}
+
+	}
+}
+
+function down()
+{
+	//Define function to move the car downward
+}
+
+function left()
+{
+	//Define function to move the car left side
+}
+
+function right()
+{
+	//Define function to move the car right side
+}
